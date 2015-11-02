1e9bac043940813d7ecbcebf702ec97a051eebf5
Maninder Singh <singhmaninder1001@gmail.com>
2015-11-02 17:48:32 +0530

    Added comments in code

--- overrideaddmethod.py
+++ overrideaddmethod.py
@@ -1,4 +1,23 @@
-# 
+""" 
+we are going to learn about how to override __add__  method while adding two objects in Python.
+
+In Python when we perform sum operation like:
+
+x + y
+
+then what actually happened is that __add__  method of object x is called in following way:
+
+x.__add__(y)
+
+If any exception comes during add operation, then reverse add operation take place in following way:
+
+y.__radd__(x)
+
+It means that we can customise the __add__ methods according to requirements.
+
+Let us understand this with the help of following simple example.
+"""
+
 class Hospital(object):
     """ This class contains number of patients that visits the Hospital in a day
     """
@@ -19,15 +38,16 @@ class Hospital(object):
         return "Number of patients: {0}".format(self.number_of_patients)
 
 
-first_day = Hospital(20)
-second_day = Hospital(30)
+if __name__ == "__main__":
+    first_day = Hospital(20)
+    second_day = Hospital(30)
 
-print first_day
-print second_day
-third_day = first_day + second_day
+    print first_day
+    print second_day
+    third_day = first_day + second_day
 
-print third_day
+    print third_day
 
-fourth_day = sum([first_day, second_day, third_day])
+    fourth_day = sum([first_day, second_day, third_day])
 
-print fourth_day
+    print fourth_day
