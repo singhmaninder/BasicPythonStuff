b7ffed79e8ceb051f06fdec5cff9df7d65c52c96
Maninder Singh <singhmaninder1001@gmail.com>
2016-02-15 17:58:23 +0530

    added exception handling for Fraction class

--- fraction.py
+++ fraction.py
@@ -5,7 +5,10 @@ class Fraction(object):
     def __init__(self, top, bottom):
         self.num = top
         self.den = bottom
-        
+
+        if not (isinstance(self.num, int) and isinstance(self.den, int)):
+            raise TypeError('Numerator and denominator must have integer values.')
+
     def __add__(self, other_fraction):
         """ This method add two Fraction objects
         """
@@ -14,24 +17,24 @@ class Fraction(object):
         common = Fraction.gcd(new_num, new_den)
         return Fraction(new_num // common, new_den // common)
         #return str(new_num) + "/" + str(new_den)
-    
+
     def __eq__(self, other):
         """ This method compare two Fraction objects
         """
         first_num = self.num * other.den
         second_num = other.num * self.den
-        
+
         return first_num == second_num
-    
+
     def __mul__(self, other):
         """ This method multiply two Fraction objects
         """
         num = self.num * other.num
         den = self.den * other.den
         common = Fraction.gcd(num, den)
-        
+
         return Fraction(num // common, den //common)
-    
+
     @staticmethod
     def gcd(m, n):
         """ This method return gcd of two numbers
@@ -43,7 +46,7 @@ class Fraction(object):
             m = old_n
             n = old_m % old_n
         return n
-    
+
     def __str__(self):
         return str(self.num) + "/" +str(self.den)
 
