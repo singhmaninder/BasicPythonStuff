3293dff9d781533b1408c07efa69661acfda2d02
Maninder Singh <singhmaninder1001@gmail.com>
2016-02-23 17:02:38 +0530

    Added a program to reverse a string using Stack data sturcture and refactor the code

---
+++ Stack/reverse_string_using_stack.py
@@ -0,0 +1,28 @@
+from stack_implementation_second_method import Stack
+
+class ReverseString(object):
+    """ This class provides implementation of reverse
+        a string
+    """
+    
+    def reverse_string(self, input_string):
+        """ This method reverse the string using Stack.
+        """
+        # create an object of Stack class.
+        s = Stack()
+        if input_string:
+            for char in input_string:
+                s.push(char)
+    
+            return ''.join(s.get_stack())
+        else:
+            return 'String is empty.'
+
+
+if __name__ == '__main__':
+    r1 = ReverseString()
+    print r1.reverse_string('Maninder Singh')
+    print r1.reverse_string('Once there was a crow')
+
+# Output: hgniS redninaM
+# worc a saw ereht ecnO
